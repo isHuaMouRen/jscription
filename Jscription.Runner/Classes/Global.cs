f07b83b25e49d1185aa7d95bcf8333c2159e7272
HuaMouRen <2825338723@qq.com>
2026-05-31 06:18:18 +0800

    Initialize

---
+++ Jscription.Runner/Classes/Global.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jscription.Runner.Classes
+{
+    public class Global
+    {
+        public const string Version = "1.0.0-Indev.1";
+    }
+}
---
+++ Jscription.Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using Jscription.Runner.Classes;
+
+namespace Jscription.Runner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Welcome to Jscription v{Global.Version}");
+        }
+    }
+}
