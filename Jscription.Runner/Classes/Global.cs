1b6a11f19ef5db79e1c27447d3207cd103530d22
HuaMouRen <2825338723@qq.com>
2026-05-31 18:24:17 +0800

    version: 1.0.0-indev.2

--- Jscription.Runner/Classes/Global.cs
+++ Jscription.Runner/Classes/Global.cs
@@ -6,6 +6,6 @@ namespace Jscription.Runner.Classes
 {
     public class Global
     {
-        public const string Version = "1.0.0-Indev.1";
+        public const string Version = "1.0.0-Indev.2";
     }
 }
