417d953a78845b32717b5ed6c47b8b67d905cdc3
HuaMouRen <2825338723@qq.com>
2026-06-01 19:50:38 +0800

    build: 在生成时启用裁剪。使程序大小从 `70MB` 左右降到了 `30MB` 左右

--- build.bat
+++ build.bat
@@ -5,13 +5,13 @@ if exist "build" (
     rd /s /q "build"
 )
 
-dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=false -p:TrimMode=copyused -o "build"
+dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=copyused -o "build"
 ren "build\Jscription.Runner.exe" "Jscription.Runner.win-x64.exe"
 
-dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r win-x86 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=false -p:TrimMode=copyused -o "build"
+dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r win-x86 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=copyused -o "build"
 ren "build\Jscription.Runner.exe" "Jscription.Runner.win-x86.exe"
 
-dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r win-arm64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=false -p:TrimMode=copyused -o "build"
+dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r win-arm64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=copyused -o "build"
 ren "build\Jscription.Runner.exe" "Jscription.Runner.win-arm64.exe"
 
 del "build\*.pdb"
