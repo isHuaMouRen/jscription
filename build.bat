e2f8e08485a399f90aebc2d00d83c16822b91efc
HuaMouRen <2825338723@qq.com>
2026-06-01 20:14:50 +0800

    build(multi-platform): 启用跨平台，现支持 `Windows` `Linux` 和 `MacOs`

--- build.bat
+++ build.bat
@@ -5,6 +5,8 @@ if exist "build" (
     rd /s /q "build"
 )
 
+:: Windows
+
 dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=copyused -o "build"
 ren "build\Jscription.Runner.exe" "Jscription.Runner.win-x64.exe"
 
@@ -14,7 +16,27 @@ ren "build\Jscription.Runner.exe" "Jscription.Runner.win-x86.exe"
 dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r win-arm64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=copyused -o "build"
 ren "build\Jscription.Runner.exe" "Jscription.Runner.win-arm64.exe"
 
-del "build\*.pdb"
+:: Linux
 
+dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=copyused -o "build"
+ren "build\Jscription.Runner" "Jscription.Runner.linux-x64"
+
+dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r linux-arm64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=copyused -o "build"
+ren "build\Jscription.Runner" "Jscription.Runner.linux-arm64"
+
+dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r linux-arm --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=copyused -o "build"
+ren "build\Jscription.Runner" "Jscription.Runner.linux-arm"
+
+:: MacOS
+
+dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r osx-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=copyused -o "build"
+ren "build\Jscription.Runner" "Jscription.Runner.macos-x64"
+
+dotnet publish "Jscription.Runner\Jscription.Runner.csproj" -c Release -r osx-arm64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=copyused -o "build"
+ren "build\Jscription.Runner" "Jscription.Runner.macos-arm64"
+
+
+
+del "build\*.pdb"
 
 pause
